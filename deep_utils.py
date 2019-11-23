6a0f5af323d81573582172ffccd2301f792a50b6
Craig Miller <craigdanielmiller@gmail.com>
2019-11-23 10:26:41 -0500

    Created pickle_dataset.py
    Created pickle_dataset.py to generate X,y pickle file pairs for each subfolder in the KITTI dataset.  Removed outdated functionality from deep_utils.

--- deep_utils.py
+++ deep_utils.py
@@ -4,10 +4,8 @@ Utility functions for working with neural networks.
 """
 from keras.models import model_from_json, model_from_yaml
 import numpy as np
-from glob import glob
 import pickle
 import matplotlib.pyplot as plt
-from image_utils import depth_read, rgb_read
 import random
 
 def save_model(model,serialize_type,model_name='model'):
@@ -63,75 +61,6 @@ def load_pickle_files(X_file, y_file):
     y=pickle.load(open(y_file,'rb'))
     return X, y
 
-def read_data(data_folderpath,num_images):
-    '''Reads full dataset.  Assumes data has been resized.
-    Assumes "data_folderpath" contains subfolders corresponding
-    to class names and each containing jpg files for class.'''
-    
-    X=np.zeros((num_images,375,1242,3),dtype=np.uint8) #Full set: 67 for image02 and 67 for image03
-    y=np.zeros((num_images,375,1242),dtype=np.uint8)
-    
-    #Append folderpaths if needed
-    if data_folderpath.endswith('\\')==False:
-        data_folderpath=str(data_folderpath)+ '\\'
-    #Establish base paths
-    x_basepath=data_folderpath+'X_rgb'+'\\'
-    y_basepath=data_folderpath+'y_depth'+'\\'
-    #Collect all foldernames for y samples
-    y_foldernames=glob(y_basepath+'*/')
- 
-    #Build full list of left and right depth images
-    left_depth_images=[]
-    right_depth_images=[]
-    y_image_names={}
-    count=0
-    for foldername in y_foldernames:
-        left_depth=foldername+'proj_depth\groundtruth\image_02\\'
-        right_depth=foldername+'proj_depth\groundtruth\image_03\\' 
-        left_depth_paths=glob(left_depth+'*.PNG')           
-        right_depth_paths=glob(right_depth+'*.PNG')
-        [left_depth_images.append(path) for path in left_depth_paths]
-        [right_depth_images.append(path) for path in right_depth_paths]
-        #Build list of image names we have depth data for
-        y_image_names[count]=[filepath.split('\\')[-1] for filepath in left_depth_paths]
-        count+=1
-
-    #Read Depth images
-    count=0
-    for depth_images in [left_depth_images,right_depth_images]:
-        #Loop through all depth images
-        for image in depth_images:
-            y[count]=depth_read(image)
-            count+=1
-
-    #Collect all foldernames for x samples
-    x_foldernames=glob(x_basepath+'*/')
-
-    #Build full list of left and right RGB images
-    left_rgb_images=[]
-    right_rgb_images=[]
-    count=0
-    for foldername in x_foldernames:
-        left_rgb=foldername+'image_02\data\\'
-        right_rgb=foldername+'image_03\data\\'
-        #Build list of paths
-        for image_name in y_image_names[count]:
-            left_rgb_images.append(left_rgb+image_name)
-            right_rgb_images.append(right_rgb+image_name)
-        count+=1
-        
-    #Read RGB images
-    count=0
-    for rgb_images in [left_rgb_images,right_rgb_images]:
-        #Loop through all rgb images
-        for image in rgb_images:
-            X[count]=rgb_read(image)
-            count+=1
-                
-    print('Pickling')
-    generate_pickle_files(X,y)
-    return X,y 
-
 def simul_shuffle(mat1, mat2):
     '''Shuffles two matrices in the same order'''
     idx=np.arange(0,mat1.shape[0])
@@ -172,5 +101,3 @@ def plot_loss(history):
     
 if __name__ == '__main__':   
     dataset=r"G:\Documents\KITTI\sandbox"
-    num_images=898
-    X,y=read_data(dataset,num_images)
---
+++ pickle_dataset.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+"""
+Reads and pickles KITTI dataset into multiple files.
+
+Expects dataset structured as follows:
+    dataset
+        X_rgb
+            2011_09_26_drive_0002_sync (default KITTI subtree)
+            2011_09_26_drive_0009_sync
+            ...
+        y_depth
+            2011_09_26_drive_0002_sync
+            2011_09_26_drive_0009_sync
+            ...
+
+Usage:
+pickle_data(dataset,output_folderpath)
+
+"""
+import numpy as np
+from glob import glob
+from image_utils import depth_read, rgb_read
+import pickle
+
+def append_folderpath(folderpath):
+    '''Adds '\\' to folderpath end if needed'''
+    if folderpath.endswith('\\')==False:
+        folderpath=str(folderpath)+ '\\'
+        
+    return folderpath
+        
+def get_X_y_paths(data_folderpath,subfolder):
+    '''Builds X & y subfolderpaths.'''
+    #Append folder paths if necessary
+    data_folderpath=append_folderpath(data_folderpath)
+    subfolder=append_folderpath(subfolder)
+    #Establish X and y subfolder paths
+    X_rgb_subpath=data_folderpath+'X_rgb'+'\\'+subfolder
+    y_depth_subpath=data_folderpath+'y_depth'+'\\'+subfolder
+    
+    return X_rgb_subpath, y_depth_subpath
+
+def get_depth_paths(y_depth_subpath):
+    '''Build lists of filepaths for left & right image depths .'''
+    y_depth_subpath=append_folderpath(y_depth_subpath)
+    #Point to correct location
+    left_depth=y_depth_subpath+'proj_depth\groundtruth\image_02\\'
+    right_depth=y_depth_subpath+'proj_depth\groundtruth\image_03\\' 
+    #Get all image filenames
+    left_depth_paths=glob(left_depth+'*.PNG')           
+    right_depth_paths=glob(right_depth+'*.PNG')
+    
+    return left_depth_paths, right_depth_paths
+    
+def pickle_depth_images(subfolder,depth_paths,output_folderpath):
+    '''Generates pickle file of y_depth data'''
+    #Read depth images and update np.array
+    num_images=len(depth_paths)
+    y=np.zeros((num_images,375,1242),dtype=np.uint8)
+    for idx, depth_path in enumerate(depth_paths):
+        y[idx]=depth_read(depth_path)
+    #Save to pickle file
+    pickle.dump(y, open(output_folderpath+r"y_"+str(subfolder)+".p", "wb"), protocol=4)
+    #Clear y variable
+    y=None
+    
+def get_rgb_paths(X_rgb_subpath,left_depth_paths, right_depth_paths):
+    '''Create list of RGB paths corresponding to input depth paths'''
+    #Point to correct location
+    left_rgb=X_rgb_subpath+'image_02\data\\'
+    right_rgb=X_rgb_subpath+'image_03\data\\'
+    #Build list of image names in left and right depth paths
+    left_depth_image_names=[filepath.split('\\')[-1] for filepath in left_depth_paths]
+    right_depth_image_names=[filepath.split('\\')[-1] for filepath in right_depth_paths]
+    #Build list of left and right RGB paths corrseponding to depth images
+    left_rgb_paths,right_rgb_paths=[],[]
+    for left_depth_image_name in left_depth_image_names:
+        left_rgb_paths.append(left_rgb+left_depth_image_name)
+    for right_depth_image_name in right_depth_image_names:
+        right_rgb_paths.append(right_rgb+right_depth_image_name)    
+    rgb_paths=left_rgb_paths+right_rgb_paths
+    
+    return rgb_paths
+ 
+def pickle_rgb_images(subfolder,rgb_paths,output_folderpath):
+    '''Generates pickle file of X_rgb data'''
+    #Read RGB images and update np.array
+    num_images=len(rgb_paths)
+    X=np.zeros((num_images,375,1242,3),dtype=np.uint8)
+    for idx, rgb_path in enumerate(rgb_paths):
+        X[idx]=rgb_read(rgb_path)
+    #Save to pickle file
+    pickle.dump(X, open(output_folderpath+r"X_"+str(subfolder)+".p", "wb"), protocol=4)
+    #Clear X variable
+    X=None
+    
+def pickle_folder(data_folderpath,subfolder,output_folderpath):
+    '''Reads and pickles one folder from KITTI.  
+    Save X, y folder pair as pickle files.'''
+    #Identify where X and y data is located    
+    X_rgb_subpath, y_depth_subpath=get_X_y_paths(data_folderpath,subfolder)
+    #Build list of filepaths for left & right image depths
+    left_depth_paths, right_depth_paths=get_depth_paths(y_depth_subpath)
+    depth_paths=left_depth_paths+right_depth_paths
+    #Create pickle file of all depth images listed in depth_paths
+    pickle_depth_images(subfolder,depth_paths,output_folderpath)
+    #Create list of corresponding RGB paths
+    rgb_paths=get_rgb_paths(X_rgb_subpath,left_depth_paths, right_depth_paths)
+    #Create pickle file of all RGB images listed in rgb_paths
+    pickle_rgb_images(subfolder,rgb_paths,output_folderpath)
+
+def pickle_dataset(data_folderpath,output_folderpath):
+    '''Reads and pickles KITTI dataset into multiple files.'''
+    output_folderpath=append_folderpath(output_folderpath)
+    #Build list of subfolders in data_folderpath\y_depth
+    data_folderpath=append_folderpath(data_folderpath)
+    y_depth_path=data_folderpath+'y_depth'
+    subfolders=glob(y_depth_path+'\\*\\')
+    #Parse out foldername
+    subfolders=[subfolder.split('\\')[-2] for subfolder in subfolders]
+    #Pickle each subfolder
+    for subfolder in subfolders:
+        print(f'Pickling {subfolder}')
+        pickle_folder(data_folderpath,subfolder,output_folderpath)
+    
+if __name__ == '__main__':   
+    dataset=r"G:\Documents\KITTI\sandbox"
+    output_folderpath=r"G:\Documents\KITTI\pickled_KITTI"
+    pickle_dataset(dataset,output_folderpath)
