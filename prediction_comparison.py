09fd2db03ca2e347b6957c9de1bb8ecc4606b122
Craig Miller <craigdanielmiller@gmail.com>
2019-12-08 20:14:59 -0500

    Update prediction_comparison.py
    Updated imports

--- prediction_comparison.py
+++ prediction_comparison.py
@@ -2,24 +2,30 @@
 """
 Script for creating depth prediction images from multiple models for comparison
 """
-import deep_utils
-import image_utils
+from utils import deep_utils
+from utils import image_utils
 import numpy as np
 
-
-models={'cnn':r"C:\Users\Craig\Documents\GitHub\depth-estimation\Final Results\200 Epochs\CNN\depth_estimation_cnn_nyu_model.yaml",
-        'wnet_c':r"C:\Users\Craig\Documents\GitHub\depth-estimation\Final Results\200 Epochs\W-Net Connected\depth_estimation_wnet_connected_nyu_model.yaml"
+#Load models (yaml/json)
+models={'unet':r"G:\WPI\Courses\2019\Deep Learning for Advanced Robot Perception, RBE595\Project\Final Results\200 Epochs\U-Net\depth_estimation_unet_nyu_model.yaml",
+        'wnet_c':r"G:\WPI\Courses\2019\Deep Learning for Advanced Robot Perception, RBE595\Project\Final Results\200 Epochs\W-Net Connected\depth_estimation_wnetc_nyu_model.yaml"
         }
 #Load weights (h5)
-weights={'cnn':r"C:\Users\Craig\Documents\GitHub\depth-estimation\Final Results\200 Epochs\CNN\depth_estimation_cnn_nyu_model.h5",
-        'wnet_c':r"C:\Users\Craig\Documents\GitHub\depth-estimation\Final Results\200 Epochs\W-Net Connected\depth_estimation_wnet_connected_nyu_model.h5"
+weights={'unet':r"G:\WPI\Courses\2019\Deep Learning for Advanced Robot Perception, RBE595\Project\Final Results\200 Epochs\U-Net\depth_estimation_unet_nyu_model.h5",
+        'wnet_c':r"G:\WPI\Courses\2019\Deep Learning for Advanced Robot Perception, RBE595\Project\Final Results\200 Epochs\W-Net Connected\depth_estimation_wnetc_nyu_model.h5"
          }
 
-images=[r"G:\Documents\NYU Depth Dataset\nyu_data\X_rgb\rgb_9.png",
-        r"G:\Documents\NYU Depth Dataset\nyu_data\X_rgb\rgb_533.png",
-        r"G:\Documents\NYU Depth Dataset\nyu_data\X_rgb\rgb_661.png",
-        r"G:\Documents\NYU Depth Dataset\nyu_data\X_rgb\rgb_854.png",
-        r"G:\Documents\NYU Depth Dataset\nyu_data\X_rgb\rgb_1204.png"]
+#images=[r"G:\Documents\NYU Depth Dataset\nyu_data\X_rgb\rgb_14.png", #9
+#        r"G:\Documents\NYU Depth Dataset\nyu_data\X_rgb\rgb_330.png", #533
+#        r"G:\Documents\NYU Depth Dataset\nyu_data\X_rgb\rgb_579.png", #661
+#        r"G:\Documents\NYU Depth Dataset\nyu_data\X_rgb\rgb_756.png", #854
+#        r"G:\Documents\NYU Depth Dataset\nyu_data\X_rgb\rgb_999.png"] #1204
+
+images=[r"G:\WPI\Courses\2019\Deep Learning for Advanced Robot Perception, RBE595\Project\Final Results\200 Epochs\Prediction Images\new_test_images\resize\1.png",
+        r"G:\WPI\Courses\2019\Deep Learning for Advanced Robot Perception, RBE595\Project\Final Results\200 Epochs\Prediction Images\new_test_images\resize\2.png",
+        r"G:\WPI\Courses\2019\Deep Learning for Advanced Robot Perception, RBE595\Project\Final Results\200 Epochs\Prediction Images\new_test_images\resize\3.png",
+        r"G:\WPI\Courses\2019\Deep Learning for Advanced Robot Perception, RBE595\Project\Final Results\200 Epochs\Prediction Images\new_test_images\resize\4.png",
+        r"G:\WPI\Courses\2019\Deep Learning for Advanced Robot Perception, RBE595\Project\Final Results\200 Epochs\Prediction Images\new_test_images\resize\5.png"]
 
 for name in models.keys():
     #Load model
