8a4aaa690c2160e608e677163a5614d78578936d
Craig Miller <craigdanielmiller@gmail.com>
2020-05-10 17:40:53 -0400

    Fixed rgb2depth cropping
    Fixed rgb2depth cropping. Deleted conv. visualization

--- models/rgb2depth.py
+++ models/rgb2depth.py
@@ -8,45 +8,47 @@ import models
 from tensorflow.keras.optimizers import Adam
   
 def rgb2depth_video(filename, model, out_FPS=10, width=640, height=192,
-                    output_filename=r'depth_output.avi', 
+                    output_filename=r'depth_output.avi', gamma=1,
                     mirror=False, crop_mode='middle'):
-    '''Create depth prediction video from input RGB video.'''
-
+    '''Create depth prediction video from input RGB video. Adjust 
+    brightness (gamma) for viewing purposes only. Anything other than 
+    gamma=1 is distorting the numerical depth prediction.'''
+        
     cam = cv2.VideoCapture(filename)
-    
+
     # Define the codec and create VideoWriter object.
     out = cv2.VideoWriter(output_filename,cv2.VideoWriter_fourcc('M','J','P','G'), 
-                          out_FPS, (int(cam.get(3)),int(cam.get(4))))
+                          out_FPS, (int(width),int(height)))
       
     while(cam.isOpened()):
         ret_val, img = cam.read()
-        if mirror: 
-            img = cv2.flip(img, 1)
-        print(img.shape)
-        if (img.shape[0] != height) or (img.shape[1] != width):
-            #Crop image
-            img=crop_image(img,width,height,mode=crop_mode)
-
-        #Predict depth
-        img=img.reshape(1,height,width,3)
-        img=np.divide(img,255).astype(np.float16) #Normalize input
-        y_est=model.predict(img)
-        y_est=y_est.reshape((height,width))
-
-        #Adjust brightness for viewing only
-        #Note that anything other than 1 is distorting the numerical
-        #depth prediction
-        gamma=0.6
         
-        #Map 2D grayscale to RGB equivalent
-        vis = cv2.cvtColor((y_est*255*(1/gamma)).astype(np.uint8),cv2.COLOR_GRAY2BGR)
-        vis = cv2.cvtColor(vis,cv2.COLOR_BGR2GRAY)
-        #Map BGR to Rainbow
-        vis=cv2.applyColorMap(vis,cv2.COLORMAP_RAINBOW)
-        
-        #Write prediction to video
-        out.write(vis)
+        try:
+            if mirror: 
+                img = cv2.flip(img, 1)
 
+            #If img doesn't match output height & width
+            if (img.shape[0] != height) or (img.shape[1] != width):
+                #Crop image
+                img=crop_image(img,width,height,mode=crop_mode)
+    
+            #Predict depth
+            img=img.reshape(1,height,width,3)
+            img=np.divide(img,255).astype(np.float16) #Normalize input
+            y_est=model.predict(img)
+            y_est=y_est.reshape((height,width))
+            
+            #Map 2D grayscale to RGB equivalent
+            vis = cv2.cvtColor((y_est*255*(1/gamma)).astype(np.uint8),cv2.COLOR_GRAY2BGR)
+            vis = cv2.cvtColor(vis,cv2.COLOR_BGR2GRAY)
+            #Map BGR to Rainbow
+            vis=cv2.applyColorMap(vis,cv2.COLORMAP_RAINBOW)
+            
+            #Write prediction to video
+            out.write(vis)
+        except:
+            break
+        
     cam.release()
     cv2.destroyAllWindows()
 
@@ -84,11 +86,13 @@ def load_model(model, weights):
     return model
 
 if __name__=='__main__':
-    #Load pretrainined & compiled model
+    #Load pretrained & compiled model
     weights=r"E:\W-Net_Connected_weights_best_KITTI_35Epochs.hdf5"
     model=models.wnet_connected()
     model=load_model(model,weights)
-    #Define input RGB video
+    #Define input RGB video (tested with .mp4, .mov, and .avi)
     rgb_video=r"G:\Program Files\MATLAB\R2018b\toolbox\vision\visiondata\atrium.mp4"
     #Create depth video
-    rgb2depth_video(rgb_video,model,mirror=False,crop_mode='middle')
+    rgb2depth_video(rgb_video,model,out_FPS=30, width=640, height=192,
+                    output_filename=r'depth_output.avi', gamma=0.6,
+                    mirror=False, crop_mode='middle')
